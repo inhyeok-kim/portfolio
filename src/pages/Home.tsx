dfcb17cf435730bdc6047a81966850944f1b4fcc
inhyeok-kim <inhyeok.kim@icloud.com>
2022-07-15 16:18:30 +0900

    1. init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+
+export default function Home(){
+
+    return (
+        <Grid container justifyContent={"center"}>
+           <Grid item xs={10} border="1px solid black">
+
+           </Grid>
+        </Grid>
+    )
+}
