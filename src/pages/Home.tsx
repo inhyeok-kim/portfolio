bea0a210cdbc9c6c40dd44fc03a2cb30c684decf
inhyeok-kim <inhyeok.kim@icloud.com>
2022-07-15 17:27:28 +0900

    1. 미디어쿼리 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 
 export default function Home(){
+    const media = useMediaQuery('(min-width:600px)');
 
     return (
         <Grid container justifyContent={"center"}>
-           <Grid item xs={10} border="1px solid black">
-
+           <Grid item xs={12} border="1px solid black">
+                {media}
+                hi
            </Grid>
         </Grid>
     )
